08b2d83523d8df276313b37e9a611c0fec3763e5
Felipe <luizfelper@gmail.com>
2021-10-25 01:31:34 -0300

    Ajustes no botão de delete

--- src/App.js
+++ src/App.js
@@ -7,14 +7,29 @@ function App() {
     Nome: 'Felipe',
     Sobrenome: 'Santos',
     Idade: '23',
-    Telefone: '+55-86-999981876'}
+    Telefone: '+55-86-999981876'
+  },
+  {
+    Id: 1,
+    Nome: 'Felipe',
+    Sobrenome: 'Santos',
+    Idade: '23',
+    Telefone: '+55-86-999981876'
+  },
+  {
+    Id: 1,
+    Nome: 'Felipe',
+    Sobrenome: 'Santos',
+    Idade: '23',
+    Telefone: '+55-86-999981876'
+  }
   ]);
- 
+
 
   return (
     <div className="App">
       <h1>Tratar dados com Javascript</h1>
-      <form onSubmit={function addPessoas(e){
+      <form onSubmit={function addPessoas(e) {
         e.preventDefault();
         const dadosPessoa = new FormData(e.target);
 
@@ -25,21 +40,47 @@ function App() {
           Idade: dadosPessoa.get('idade'),
           Telefone: dadosPessoa.get('telefone')
         };
-      
+
         const pessoasAtualizadas = [...dados, obtemDados]
         setDados(pessoasAtualizadas);
         console.log(pessoasAtualizadas);
       }}>
-        <input type="text" name="nome" placeholder="Nome"/>
-        <input type="text" name="sobrenome" placeholder="Sobrenome"/>
-        <input type="text" name="idade" placeholder="Idade"/>
-        <input type="text" name="telefone" placeholder="Telefone"/>
+        <input type="text" name="nome" placeholder="Nome" />
+        <input type="text" name="sobrenome" placeholder="Sobrenome" />
+        <input type="text" name="idade" placeholder="Idade" />
+        <input type="text" name="telefone" placeholder="Telefone" />
         <button>Cadastrar Aluno</button>
       </form>
-      
+      <form>
+        <input type="text" name="buscar" placeholder="Buscar" />
+      </form>
+
       <h2>Quantidade ({dados.length})</h2>
-      
+
+      {dados.map((itemAtual, i) => {
+        return (
+          <div className="ItemAluno" key={itemAtual.Id}>
+            <p>Nome: {itemAtual.Nome}</p>
+            <p>Sobrenome: {itemAtual.Sobrenome}</p>
+            <p>Idade: {itemAtual.Idade}</p>
+            <p>Telefone: {itemAtual.Telefone}</p>
+            <div>
+              <form onSubmit={ function excluir(e) {
+                e.preventDefault();
+                delete dados[i];
+                setDados(dados);
+                console.log(dados);
+              }}>
+              <button>Remover</button>
+              </form>
+ 
+            </div>
+          </div>
+
+        )
+      })}
+
     </div>
   );
-    }
+}
 export default App;
