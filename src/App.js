2415cc3a9eae8e2923a4e8729441ee132c212f74
Felipe <luizfelper@gmail.com>
2021-10-25 02:39:04 -0300

    Ajustes e exclusão do Footer

--- src/App.js
+++ src/App.js
@@ -28,68 +28,65 @@ function App() {
 
   return (
     <div>
-    <div className="App">
-      <h1>Tratar dados com Javascript</h1>
-      <form className="boxCadastrar" onSubmit={function addPessoas(e) {
-        e.preventDefault();
-        const dadosPessoa = new FormData(e.target);
+      <div className="App">
+        <h1>Tratar dados com Javascript</h1>
+        <form className="boxCadastrar" onSubmit={function addPessoas(e) {
+          e.preventDefault();
+          const dadosPessoa = new FormData(e.target);
 
-        const obtemDados = {
-          Id: 1,
-          Nome: dadosPessoa.get('nome'),
-          Sobrenome: dadosPessoa.get('sobrenome'),
-          Idade: dadosPessoa.get('idade'),
-          Telefone: dadosPessoa.get('telefone')
-        };
+          const obtemDados = {
+            Id: 1,
+            Nome: dadosPessoa.get('nome'),
+            Sobrenome: dadosPessoa.get('sobrenome'),
+            Idade: dadosPessoa.get('idade'),
+            Telefone: dadosPessoa.get('telefone')
+          };
 
-        const pessoasAtualizadas = [...dados, obtemDados]
-        setDados(pessoasAtualizadas);
-        console.log(pessoasAtualizadas);
-      }}>
-        <input type="text" name="nome" placeholder="Nome" />
-        <input type="text" name="sobrenome" placeholder="Sobrenome" />
-        <input type="number" name="idade" placeholder="Idade" />
-        <input type="number" name="telefone" placeholder="Telefone" />
-        <div className="boxBtnCadastrar">
-          <button className="btnCad">Cadastrar</button>
-          <button className="btnLimpa">Limpar</button>
-        </div>
-      </form>
-      <form>
-        <input type="text" name="buscar" placeholder="Buscar" />
-      </form>
-
-      <h2>Quantidade ({dados.length})</h2>
-
-      {dados.map((itemAtual, i) => {
-        return (
-          <div className="ItemAluno" key={itemAtual.i}>
-            <p>Nome: {itemAtual.Nome}</p>
-            <p>Sobrenome: {itemAtual.Sobrenome}</p>
-            <p>Idade: {itemAtual.Idade}</p>
-            <p>Telefone: {itemAtual.Telefone}</p>
-            <div>
-              <form>
-                <button onClick={function excluir(e) {
-                  e.preventDefault();
-                  delete dados[i];
-                  const dadosAtuais = dados.filter(item => item !== null);
-                  setDados(dadosAtuais);
-                  console.log(dados);
-                }}>Remover</button>
-              </form>
-
-            </div>
+          const pessoasAtualizadas = [...dados, obtemDados]
+          setDados(pessoasAtualizadas);
+          console.log(pessoasAtualizadas);
+        }}>
+          <input type="text" name="nome" placeholder="Nome" />
+          <input type="text" name="sobrenome" placeholder="Sobrenome" />
+          <input type="number" name="idade" placeholder="Idade" />
+          <input type="number" name="telefone" placeholder="Telefone" />
+          <div className="boxBtnCadastrar">
+            <button className="btnCad">Cadastrar</button>
+            <button className="btnLimpa">Limpar</button>
           </div>
+        </form>
+        <form>
+          <input type="text" name="buscar" placeholder="Buscar" />
+        </form>
 
-        )
-      })}
-    </div>
-    <div className="footer">
-      <p>Aplicação em ReactJS criado por Felipe Fontenele
-        github.com/luizfelper
-      </p>
-    </div>
+        <h2>Quantidade ({dados.length})</h2>
+
+        <div className="boxListas">
+          {dados.map((itemAtual, i) => {
+            return (
+              <div className="ItemAluno" key={itemAtual.i}>
+                <p>Nome: {itemAtual.Nome}</p>
+                <p>Sobrenome: {itemAtual.Sobrenome}</p>
+                <p>Idade: {itemAtual.Idade}</p>
+                <p>Telefone: {itemAtual.Telefone}</p>
+                <div>
+                  <form>
+                    <button onClick={function excluir(e) {
+                      e.preventDefault();
+                      delete dados[i];
+                      const dadosAtuais = dados.filter(item => item !== null);
+                      setDados(dadosAtuais);
+                      console.log(dados);
+                    }}>Remover</button>
+                  </form>
+
+                </div>
+              </div>
+
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
